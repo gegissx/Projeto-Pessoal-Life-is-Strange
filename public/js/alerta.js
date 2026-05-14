97e5d20eff9b8de6f4a53d73593441f77ad2f8bb
gegissx <geovanna.paschoal@sptech.school>
2026-05-14 00:20:24 -0300

    Atualização na dashboard com lógica para mostrar porcentagem na legenda do gráfico. Inserção da API na área de Cadastro e Login

--- public/js/alerta.js
+++ public/js/alerta.js
@@ -1,24 +1,6 @@
 var alertas = [];
 
-function obterdados(idAquario) {
-    fetch(`/medidas/tempo-real/${idAquario}`)
-        .then(resposta => {
-            if (resposta.status == 200) {
-                resposta.json().then(resposta => {
 
-                    console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
-
-                    alertar(resposta, idAquario);
-                });
-            } else {
-                console.error(`Nenhum dado encontrado para o id ${idAquario} ou erro na API`);
-            }
-        })
-        .catch(function (error) {
-            console.error(`Erro na obtenção dos dados do aquario p/ gráfico: ${error.message}`);
-        });
-
-}
 
 function alertar(resposta, idAquario) {
     var temp = resposta[0].temperatura;
